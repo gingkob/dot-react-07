d85ff137b58cde91c121d02d21e094556f02d159
gingkob <malivlada@hotmail.com>
2021-02-14 21:33:08 +0100

    manage state in class based component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,28 +4,38 @@ import './Product.css';
 export class Product extends Component {
 
   state = {
-    items: 0
+    cart: [],
+    total: 0
   }
 
   onAdd = () => {
-    this.setState( prevState =>({ items: prevState.items + 1 }))
+    this.setState( prevState =>({ cart: [...prevState.cart, 'ice cream'], total: prevState.total + 5}))
   }
 
   onRemove = () => {
-    if(this.state.items === 0){
+    if(this.state.cart.length === 0){
       return;
     }
-    this.setState( prevState =>({ items:prevState.items - 1}))
+    this.setState( prevState =>({ cart: prevState.cart.slice(0,-1), total: prevState.total - 5}))
+  }
+
+  currencyOptions = {
+    minimumFractionDigits:2,
+    maximumFractionDigits:2
+  }
+
+  getTotal = () =>{
+    return this.state.total.toLocaleString("en-US", this.currencyOptions)
   }
 
   render() {
     return (
       <div className="product-wrapper">
         <div className='flexible'>
-          <span>Shopping Cart: </span><span>{this.state.items} total items.</span>
+          <span>Shopping Cart: </span><span>{this.state.cart.length} total items.</span>
         </div>
         <div className='flexible'>
-          <span>Total: </span><span>{this.state.items * 5} $</span>
+          <span>Total: </span><span>{this.getTotal()} $</span>
           </div>
 
         <div className="product">
